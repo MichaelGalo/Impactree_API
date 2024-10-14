a1c4232888fd8ad9a8a871763d736c71f121762a
Michael Galo <michael@michaelthinks.co>
2024-10-14 10:34:43 -0500

    Initial commit

---
+++ impactreeapi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ImpactreeapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'impactreeapi'
---
+++ seed_database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./impactreeapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations impactreeapi
+python3 manage.py migrate impactreeapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+
