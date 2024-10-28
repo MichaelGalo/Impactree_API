fbdd45de8d7ad9c6e407c6c1bdd5df81e7350d6a
Michael Galo <michael@michaelthinks.co>
2024-10-28 10:49:00 -0500

    Added automated test suite to impactplan_charity

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,3 +4,4 @@ from .milestone import MilestoneViewSetTests
 from .charitycategory import CharityCategoryViewSetTests
 from .charity import CharityViewSetTests
 from .impactplan import ImpactPlanViewTests
+from .impactplan_charity import ImpactPlanCharityViewSetTests
